dcd7a10e3ad8d6431f772fae055f6166a53d56b5
rudovc <roko.udovc@outlook.com>
2025-06-19 19:34:24 +0200

    Add ACC engine maps

--- JavascriptExtensions/universalDashGetters.js
+++ JavascriptExtensions/universalDashGetters.js
@@ -612,6 +612,12 @@ const AC1_ERS_MODE_LABEL_MAP = {
   5: "Quali",
 };
 /** @type {StringRecord} */
+/**
+ * Aston Martin V12 and V8 GT3
+ * Audi R8 GT3 and GT3 EVO
+ * Bentley Continental GT3 2016 and 2018
+ * Lamborghini Huracan GT3 and GT3 EVO
+ */
 const ACC_DRY_WET_PACE_CAR_MAPS = {
   1: "1 - DRY FAST",
   2: "2 - DRY",
@@ -622,12 +628,186 @@ const ACC_DRY_WET_PACE_CAR_MAPS = {
   7: "7 - WET FUEL SAVE",
   8: "8 - SAFETY CAR",
 };
+/**
+ * Ferrari 488 GT3
+ */
+const ACC_FERRARI_GT3_MAPS = {
+  1: "1 - DRY FAST",
+  2: "2 - DRY FUEL SAVE",
+  3: "3 - DRY",
+  4: "4 - DRY SLOW",
+  5: "5 - WET",
+  6: "6 - WET FUEL SAVE",
+  7: "7 - WET",
+  8: "8 - WET SLOW",
+  9: "9 - SAFETY CAR",
+  10: "10 - SAFETY CAR",
+  11: "11 - SAFETY CAR",
+  12: "12 - SAFETY CAR",
+};
+/**
+ * Ferrari 488 GT3 EVO
+ */
+const ACC_FERRARI_GT3_EVO_MAPS = {
+  1: "1 - DRY FAST",
+  2: "2 - DRY FAST",
+  3: "3 - DRY",
+  4: "4 - DRY FUEL SAVE",
+  5: "5 - DRY FUEL SAVE",
+  6: "6 - WET FUEL SAVE",
+  7: "7 - WET",
+  8: "8 - WET SLOW",
+  9: "9 - SAFETY CAR",
+  10: "10 - SAFETY CAR",
+  11: "11 - SAFETY CAR",
+  12: "12 - SAFETY CAR",
+};
+/**
+ * BMW M6 and M4 GT3
+ */
+const ACC_BMW_MAPS = {
+  1: "1 - DRY FAST",
+  2: "2 - DRY",
+  3: "3 - DRY",
+  4: "4 - RESERVE",
+  5: "5 - SAFETY CAR",
+  6: "6 - WET FAST",
+  7: "7 - WET",
+  8: "8 - WET",
+};
+/**
+ * HONDA NSX GT3 and GT3 EVO
+ */
+const ACC_HONDA_MAPS = {
+  1: "1 - DRY PROG",
+  2: "2 - DRY LINEAR",
+  3: "3 - DRY AGGR",
+  4: "4 - DRY MAX",
+  5: "5 - WET PROG",
+  6: "6 - WET PROG",
+  7: "7 - WET FUEL SAVE",
+  8: "8 - SAFETY CAR",
+};
+/**
+ * LAMBORGHINI GALLARDO R-EX
+ */
+const ACC_GALLARDO_MAPS = {
+  1: "1 - FAST",
+  2: "2 - AGGR",
+  3: "3 - PROG",
+  4: "4 - FUEL SAVE",
+};
+/**
+ * JAGUAR
+ */
+const ACC_JAGUAR_MAPS = {
+  1: "1 - DRY FAST",
+  2: "2 - DRY",
+  3: "3 - DRY FUEL SAVE",
+  4: "4 - WET FAST",
+  5: "5 - WET",
+  6: "6 - WET FUEL SAVE",
+};
+/**
+ * NISSAN GT-R GT3 2016 and 2018
+ * MERCEDES AMG GT3 and GT3 2020
+ */
+const ACC_SHORT_MAPS = {
+  1: "1 - DRY FAST",
+  2: "2 - DRY",
+  3: "3 - DRY FUEL SAVE",
+  4: "4 - FUEL SAVE MODE",
+};
+/**
+ * LEXUS RC F GT3
+ */
+const ACC_LEXUS_MAPS = {
+  1: "1 - DRY LINEAR",
+  2: "2 - DRY AGGR",
+  3: "3 - DRY PROG",
+  4: "4 - FUEL SAVE / WET",
+  5: "5 - SAFETY CAR",
+};
+/**
+ * MCLAREN 650S GT3
+ */
+const ACC_650S_MAPS = {
+  1: "1 - DRY FAST",
+  2: "2 - DRY",
+  3: "3 - DRY",
+  4: "4 - DRY FUEL SAVE",
+  5: "5 - WET FAST",
+  6: "6 - WET FUEL SAVE",
+  7: "7 - WET",
+  8: "8 - WET FUEL SAVE",
+  9: "9 - SAFETY CAR",
+};
+/**
+ * MCLAREN 720S GT3
+ */
+const ACC_720S_MAPS = {
+  1: "1 - QUALI",
+  2: "2 - RACE 1",
+  3: "3 - RACE 2",
+  4: "4 - RACE 1 FUEL SAVE",
+  5: "5 - RACE 2 FUEL SAVE",
+  6: "6 - RACE 3 FUEL SAVE",
+  7: "7 - ENGINE HI TEMP",
+  8: "8 - DAMP QUALI",
+  9: "9 - DAMP RACE FUEL SAVE",
+  10: "10 - WET QUALI",
+  11: "11 - WET RACE 1",
+  12: "12 - WET RACE 2",
+};
+/**
+ * PORSCHE 911 GT3-R and GT3-R EVO
+ */
+const ACC_PORSCHE_MAPS = {
+  1: "1 - DRY",
+  2: "2 - DRY PROG",
+  3: "3 - DRY AGGR",
+  4: "4 - WET",
+  5: "5 - QUALI",
+  6: "6 - QUALI PROG",
+  7: "7 - QUALI AGGR",
+  8: "8 - DRY QUALI",
+  9: "9 - FUEL SAVE",
+  10: "10 - SAFETY CAR",
+};
+
 /** @type {NullableGameOrCarClassLabelMapRecord} */
 const ERS_MODE_LABEL_MAP = {
   Automobilista2: new LabelMap(AMS_ERS_MODE_LABEL_MAP),
   AssettoCorsa: new LabelMap(AC1_ERS_MODE_LABEL_MAP),
   AssettoCorsaCompetizione: {
-    ferrari_488_gt3: new LabelMap(ACC_DRY_WET_PACE_CAR_MAPS),
+    amr_v12_vantage_gt3: new LabelMap(ACC_DRY_WET_PACE_CAR_MAPS),
+    audi_r8_lms_evo: new LabelMap(ACC_DRY_WET_PACE_CAR_MAPS),
+    bentley_continental_gt3_2018: new LabelMap(ACC_DRY_WET_PACE_CAR_MAPS),
+    bmw_m6_gt3: new LabelMap(ACC_BMW_MAPS),
+    ferrari_488_gt3: new LabelMap(ACC_FERRARI_GT3_MAPS),
+    honda_nsx_gt3_evo: new LabelMap(ACC_HONDA_MAPS),
+    lamborghini_huracan_gt3_evo: new LabelMap(ACC_DRY_WET_PACE_CAR_MAPS),
+    lamborghini_huracan_gt3: new LabelMap(ACC_DRY_WET_PACE_CAR_MAPS),
+    mclaren_650s_gt3: new LabelMap(ACC_650S_MAPS),
+    mclaren_720s_gt3: new LabelMap(ACC_720S_MAPS),
+    mercedes_amg_gt3: new LabelMap(ACC_SHORT_MAPS),
+    nissan_gt_r_gt3_2018: new LabelMap(ACC_SHORT_MAPS),
+    porsche_911ii_gt3_r: new LabelMap(ACC_PORSCHE_MAPS),
+
+    // Not tested: I do not own these cars for ACC. The keys might need to be renamed to match the in-game car ID
+    amr_v8_vantage_gt3: new LabelMap(ACC_DRY_WET_PACE_CAR_MAPS),
+    audi_r8_lms_evoii: new LabelMap(ACC_DRY_WET_PACE_CAR_MAPS),
+    bentley_continental_gt3_2015: new LabelMap(ACC_DRY_WET_PACE_CAR_MAPS),
+    bmw_m4_gt3: new LabelMap(ACC_BMW_MAPS),
+    emil_frey_jaguar_gt3: new LabelMap(ACC_JAGUAR_MAPS),
+    ferrari_488_gt3_evo: new LabelMap(ACC_FERRARI_GT3_EVO_MAPS),
+    honda_nsx_gt3: new LabelMap(ACC_HONDA_MAPS),
+    lexus_rc_f_gt3: new LabelMap(ACC_LEXUS_MAPS),
+    mclaren_720s_gt3_evo: new LabelMap(ACC_720S_MAPS),
+    mercedes_amg_gt3_evo: new LabelMap(ACC_SHORT_MAPS),
+    nissan_gt_r_gt3_2015: new LabelMap(ACC_SHORT_MAPS),
+    porsche_911_gt3_r: new LabelMap(ACC_PORSCHE_MAPS),
+    reiter_engineering_r_ex_gt3: new LabelMap(ACC_GALLARDO_MAPS),
   },
   LMU: null,
 };
@@ -925,6 +1105,23 @@ const ABS_GAME_PROPERTY_MAP = {
   Generic: "ABSLevel",
   LMU: "LMU_NeoRedPlugin.Extended.VM_ANTILOCKBRAKESYSTEMMAP",
 };
+/** @type {StringRecord} */
+const LMU_ABS_LABEL_MAP = {
+  0: "Off",
+  1: "o 1 (LO)",
+  2: "o 1 (MD)",
+  3: "o 1 (HI)",
+  4: "n 4 (LO)",
+  5: "n 5 (MD)",
+  6: "n 6 (HI)",
+  7: "u 7 (LO)",
+  8: "u 8 (MD)",
+  9: "u 9 (HI)",
+};
+/** @type {GameOrCarClassLabelMapRecord} */
+const ABS_LABEL_MAP = {
+  LMU: new LabelMap(LMU_ABS_LABEL_MAP),
+};
 /** @type {HigherOrderFunctionRecord} */
 const ABS_TRANSFORMATION_MAP = {
   "LMU_NeoRedPlugin.Extended.VM_ANTILOCKBRAKESYSTEMMAP":
@@ -934,18 +1131,12 @@ const ABS_TRANSFORMATION_MAP = {
      */
     (rawAbsLevel) => {
       if (rawAbsLevel.includes("OFF")) {
-        return "OFF";
+        return 0;
       }
 
-      const labelIndex = rawAbsLevel.indexOf("(");
-      const label = rawAbsLevel[labelIndex + 1];
       const absLevel = Number.parseInt(rawAbsLevel);
 
-      if (!absLevel) {
-        return "OFF";
-      }
-
-      return labelIndex > 0 ? `${absLevel} (${label})` : absLevel;
+      return absLevel;
     },
 };
 /** @type {GameOrCarClassNullableStringRecord} */
